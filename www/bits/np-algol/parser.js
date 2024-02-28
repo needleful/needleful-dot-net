5e2aeebf517e30474282be751affc4d77d446775
needleful <needleful@outlook.com>
2024-02-27 21:47:47 -0800

    Expression parsng seems to work

--- www/bits/np-algol/parser.js
+++ www/bits/np-algol/parser.js
@@ -8,14 +8,42 @@ function parseAlgol(text, options = {}) {
 		types: ['real', 'integer', 'Boolean'],
 		procedure: 'procedure',
 		semicol: ';',
+		cond_if: 'if',
+		cond_then: 'then',
+		cond_else: 'else',
 		comma: ',',
 		assign: ':=',
+		parenOpen: '(',
+		parenClose: ')',
 		postComment: /^([^;]*);?/,
 		identPart: /^\p{Alpha}[\p{Alpha}\d]*/u,
 		ofType:/^(real|integer|Boolean)/,
-		anyToken:/^\S+/
+		anyToken:/^\S+/,
+		sign:/^(\+|-)/,
+		decimal:'.',
+		digits: /^\d+/,
+		exponent:'_e',
+		signedExponent: /^_e(\+|-)?/,
+		logicalVal:/^(true|false)/,
+		// From highest to lowest in the parsing tree, binary ops only
+		logicOps:[
+			// Logical operators
+			'is', // Logical equivalence, not numeric equality
+			'implies', // Implication
+			'or', 
+			'and',
+		],
+		logicalNot: '!',
+		relationOps:/^(<|<=|=|>=|>|!=)/,
+		arithmeticOps:[
+			// Arithmetic
+			/^(\+|-)/,	
+			/^(\*|\/|%)/,
+			/^\^/
+		],
 	};
-	const keywords = Object.values(Pc).flat();
+	// Forgive me
+	const keywords = Object.values(Pc).flat().filter(e => typeof(e) == 'string');
 
 	function getPositions(...indeces) {
 		let result = {};
@@ -42,6 +70,7 @@ function parseAlgol(text, options = {}) {
 	}
 
 	function perr(location, text) {
+		console.trace();
 		let [loc] = getPositions(location);
 		throw {text:text, location:loc};
 	}
@@ -57,8 +86,11 @@ function parseAlgol(text, options = {}) {
 		return c < text.length;
 	}
 
-	function declaration(type, own = false) {
-		let vars = [];
+	function identifier() {
+		return grab(Pc.identPart);
+	}
+
+	function stroppedIdentifier() {
 		function checkWord(word) {
 			if (keywords.includes(word)) {
 				pwarn(c, `Variable name includes keyword: {${word}}. Is that correct?`);
@@ -66,69 +98,261 @@ function parseAlgol(text, options = {}) {
 			}
 			return false;
 		}
+		let word = grab(Pc.identPart);
+		if(!word) {
+			return null;
+		}
+		checkWord(word, options);
+		let wordEnd = false;
+		let wordParts = 1;
+		while(good() && !wordEnd) {
+			let w2 = grab(Pc.identPart);
+			if(w2) {
+				checkWord(w2);
+				word += w2;
+				wordParts += 1;
+			}
+			else {
+				wordEnd = true;
+			}
+		}
+		if(wordParts > 1) {
+			pwarn(c, `Identifier contains whitespace. Reading as {${word}}`);;
+		}
+		return word;
+	}
+
+	function parseBinary(ops, top, primary, index = 0) {
+		if(index >= ops.length) {
+			return primary();
+		}
+		let op = ops[index];
+		let lhs;
+		if(grab(Pc.parenOpen)) {
+			lhs = top();
+			grabOrDie(Pc.parenClose, 'Expected a closing parenthesis for the nested expression');
+		}
+		else{
+			lhs = parseBinary(ops, top, primary, index + 1);
+		}
+		if(!lhs) {
+			return null;
+		}
+
+		let foundOp = grab(op);
+		if(foundOp) {
+			let rhs = expect(parseBinary(ops, top, primary, index + 1), `Missing the right-hand side of {${foundOp}}`);
+			return {op: foundOp, left:lhs, right:rhs}
+		}
+		else {
+			return lhs;
+		}
+	}
+
+	function ifClause() {
+		if(grab(Pc.cond_if)) {
+			let condition = expect(boolean(), `Expected a boolean expression after {if}`);
+			return condition;
+		}
+		else {
+			return null;
+		}
+	}
+
+	function digits() {
+		let val = '';
+		while(peek(Pc.digits)) {
+			val += grab(Pc.digits);
+		}
+		return val.length? val : null;
+	}
+
+	function arithmeticPrimary() {
+		let val = identifier();
+		if (val) {
+			return val;
+		}
+		else {
+			let sign = grab(Pc.sign);
+			let uint = digits();
+			let decimal = grab(Pc.decimal);
+			let lowDigits = decimal ? 
+				expect(digits(), 'Digits are always expected after a decimal point') 
+				: digits();
+			let exp = grab(Pc.signedExponent);
+			let expDigits = exp ? 
+				expect(digits(), 'A value is required for the exponent') 
+				: digits();
+
+			val = [sign, uint, decimal, lowDigits, exp, expDigits].filter(e => e).join('');
+			if(exp && !uint && !lowDigits) {
+				val = val.replace(Pc.exponent, '1'+Pc.exponent);
+			}
+			let result = Number(val.replace(Pc.exponent, 'e'));
+			if(isNaN(result)) {
+				perr(c, `Not a valid number: {${val}}`);
+			}
+			return result;
+		}
+	}
+
+	function comparison() {
+		let lhs = arithmetic();
+		if(!lhs) { return null; }
+		let op = grab(Pc.relationOps);
+		if(!op) {return lhs;}
+		let rhs = expect(arithmetic(), "Expression expected on the right side of a comparison.");
+		return {op: op, lhs:lhs, rhs: rhs}
+	}
+
+	function booleanPrimary() {
+		let not_op = grab(Pc.logical_not);
+		let val = comparison();
+		if(!val) {
+			val = grab(Pc.logicalVal);
+			val = val? Boolean(val) : identifier();
+		}
+		if(typeof(val) == 'number' || 
+			typeof(val) == 'object' && 'op' in val && !val.op.match(Pc.relationOps)) 
+		{
+			perr(c, 'Expected a boolean expression, found arithmetic instead');
+		}
+		if(not_op) {
+			return {op: not_op, arg: expect(val)};
+		}
+		else {
+			return val;
+		}
+	}
+
+	function simpleArithmetic() {
+		return parseBinary(Pc.arithmeticOps, arithmetic, arithmeticPrimary);
+	}
+
+	function simpleBoolean() {
+		return parseBinary(Pc.logicOps, boolean, booleanPrimary);
+	}
+
+	function ifElse(lhs, rhs) {
+		let lif = ifClause();
+		if(lif) {
+			grabOrDie(Pc.cond_then);
+			let then_do = expect(lhs(), `No condition after then-clause for conditional expression`);
+			grabOrDie(Pc.cond_else, "{else} clause is mandatory for conditional expressions.");
+
+			let else_do = expect(arithmetic(), `No expression found after {else}`);
+			
+			return {
+				cond:lif, 
+				then_do:then_do, 
+				else_do:else_do
+			};
+		}
+		else {
+			return lhs();
+		}
+	} 
+
+	function arithmetic() {
+		return ifElse(simpleArithmetic, arithmetic);
+	}
+
+	function boolean() {
+		return ifElse(simpleBoolean, boolean);
+	}
+
+	function declaration(type, own = false) {
+		let vars = [];
 		let declEnd = false;
-		while(good() && !declEnd) {
-			let word = grabToken(Pc.identPart);
-			if(!word) {
-				if(grabToken(Pc.comma)) {
-					perr(c, `Empty item (or extra comma) in declarations list`);
-				}
-				else if(grabToken(Pc.semicol)) {
+		while(good()) {
+			let nextVar = identifier();
+			if(!nextVar) {
+				 if(grab(Pc.semicol)) {
 					perr(c, `Declarations list can't be empty.`);
 				}
-				else{
-					perr(c, `Expected an identifier, found {${grabToken(Pc.anyToken)}}`);
-				}
-			}
-			checkWord(word, options);
-			let wordEnd = false;
-			while(good() && !wordEnd) {
-				let w2 = grabToken(Pc.identPart);
-				if(w2) {
-					checkWord(w2);
-					word += w2;
-				}
-				else if(grabToken(Pc.comma)){
-					wordEnd = true;
-				}
-				else if(grabToken(Pc.semicol)){
-					wordEnd = true;
-					declEnd = true;
+				else if(grab(Pc.comma)) {
+					perr(c, `Empty item (or extra comma) in declarations list`);
 				}
 				else {
-					perr(c, `Unexpected token during variable declaration: {${grabToken(Pc.anyToken)}}`);
+					perr(c, `Expected an identifier, found {${grab(Pc.anyToken)}}`);
 				}
 			}
-			vars.push(word);
+			vars.push(nextVar);
+
+			if(grab(Pc.semicol)){
+				break;
+			}
+			else {
+				grabOrDie(Pc.comma);
+			}
 		}
 		return {decl: type, own: own, vars:vars};
+	}
+
+	function assignment(firstVar) {
+		let result = {vars:[firstVar]};
+		while(good()) {
+			if(grab(Pc.semicol) || peek(Pc.end)) {
+				perr(c, `Assignment to {${firstVar}} without a value on the right-hand side`);
+			}
+			let next = arithmetic();
+			if(typeof(next) == 'string') {
+				if (grab(Pc.assign)) {
+					result.vars.push(next);
+					continue;
+				}
+				else if(grab(Pc.semicol) || peek(Pc.end)) {
+					result.value = next;
+					break;
+				}
+				else {
+					perr(c, `Unexpected text in assignment {${result.vars} := ${next}}: {${grab(Pc.anyToken)}}`);
+				}
+			}
+			else if(next) {
+				result.value = next;
+				if(grab(Pc.semicol) || peek(Pc.end)) {
+					break;
+				}
+				else if(grab(Pc.assign)) {
+					perr(c, `Cannot assign more variables after expression {${next}}`);
+				}
+				else {
+					perr(c, `Unexpected tokens after assignment {${result.vars} := ${next}}: {${grab(Pc.anyToken)}}. Missing semicolon?`)
+				}
+			}
+			else {
+				perr(c, 'Invalid arithmetic expression in assignment');
+			}
+		}
+		return result;
 	}
 
 	function blockHead() {
 		let declarations = [];
 		while(good()) {
-			if(peekToken(Pc.begin) || peekToken(Pc.end)) {
+			if(peek(Pc.begin) || peek(Pc.end)) {
 				break;
 			}
-			let word = grabToken(/^\p{Alpha}+/u);
+			let word = grab(/^\p{Alpha}+/u);
 			if(word == Pc.own) {
-				let next = grabToken(Pc.ofType);
-				if (!next){
-					perr(c, `Expected one of [${Pc.types}] after {own}. Found {${next}}`)
-				}
+				let next = expect(grab(Pc.ofType), 
+					`Expected one of [${Pc.types}] after {own}. Found {${next}}`)
 				declarations.push(declaration(next, true));
 			}
 			else if(word == Pc.comment) {
-				grabToken(Pc.postComment);
+				grab(Pc.postComment);
 			}
 			else if(Pc.types.includes(word)) {
 				declarations.push(declaration(word));
 			}
-			else if(grabToken(Pc.semicol)) {
+			else if(word) {
+				// Let the block tail figure it out
+				backtrack(word);
 				break;
 			}
-			else {
-				perr(c, 'Unexpected token in block head: '+word);
+			else if(grab(Pc.semicol)) {
+				continue;
 			}
 		}
 		return declarations;
@@ -138,7 +362,7 @@ function parseAlgol(text, options = {}) {
 		let block = [];
 		let ended = false;
 		while(good()) {
-			let word = grabToken(Pc.identPart);
+			let word = grab(Pc.identPart);
 			if(word == Pc.own) {
 				perr(c, "Cannot declare new variables after the block head");
 			}
@@ -148,7 +372,7 @@ function parseAlgol(text, options = {}) {
 				block.push({head:head, tail:tail});
 			}
 			else if(word == Pc.end) {
-				let comment = grabToken(Pc.postComment);
+				let comment = grab(Pc.postComment);
 				if(!comment) {
 					pwarn(c, `Expected a semicolon after 'end'.`);
 				}
@@ -158,38 +382,35 @@ function parseAlgol(text, options = {}) {
 				ended = true;
 				break;
 			}
+			else if(word) {
+				if(grab(Pc.assign)) {
+					block.push(assignment(word));
+				}
+			}
+			else if(grab(Pc.semicol)){
+				// Dummy statement
+				continue;
+			}
 			else {
-				pwarn(c, "I don't know what this means in this context: ", word);
+				pwarn(c, "Unexpected text: ", grab(Pc.anyToken));
 			}
 		}
-		if(!ended) {
-			perr(c, 'Document ended with more `begin` than `end` brackets.');
-		}
+		expect(ended, 'Document ended with more `begin` than `end` brackets.');
 		return block;
 	}
 
 	function skipWhiteSpace(){
-		let s1 = text.substr(c, 10);
 		while(c < text.length && text[c].match(/\s/)) {
 			c++;
 		}
 	}
 
-	// peek at the token, and advance if it exists.
-	function grabToken(query) {
-		let r = peekToken(query);
-		if(r) {
-			c += r.length;
-		}
-		return r;
-	}
-
 	// Match string or regex
 	// Regexes should have ^ at the front
-	function peekToken(query) {
+	function peek(query) {
 		skipWhiteSpace();
 		if(typeof(query) == 'string') {
-			return text.startsWith(query, c) ? query : false;
+			return text.startsWith(query, c) ? query : null;
 		}
 		else {
 			let m = text.substr(c).match(query);
@@ -197,9 +418,39 @@ function parseAlgol(text, options = {}) {
 		}
 	}
 
-	if(!grabToken(Pc.begin)) {
-		perr(c, 'Expected keyword {begin} to start the program.');
+	// peek at the token, and advance if it exists.
+	function grab(query) {
+		let r = peek(query);
+		if(r) {
+			c += r.length;
+		}
+		return r;
 	}
+
+	function grabOrDie(query, error) {
+		var g = grab(query);
+		if (!g) {
+			perr(c, `Expected token {${query}} found {${grab(Pc.anyToken)}}, ${error? error : ""}`);
+		}
+		return g;
+	}
+
+	function expect(expression, error) {
+		if(!expression) {
+			perr(c, error);
+		}
+		return expression;
+	}
+
+	function backtrack(token) {
+		let end = text.substr(c-token.length, token.length);
+		if(end != token) {
+			perr(c, `PARSER BUG: Tried to backtrack on token {${token}}, but the value was {${end}}` );
+		}
+		c -= token.length;
+	}
+
+	grabOrDie(Pc.begin, "Keyword {begin} is required at the start of a program.");
 	let head = blockHead();
 	let tail = blockTail();
 	return {head: head, tail: tail};
